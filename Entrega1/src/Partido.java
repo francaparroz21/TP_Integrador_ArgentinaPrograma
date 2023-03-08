0906084867ea291f44ed145f078b8c331f349a1d
francaparroz21 <francaparroz21@gmail.com>
2023-03-08 18:17:22 -0300

    agregando metodos de resultados.

--- Entrega1/src/Partido.java
+++ Entrega1/src/Partido.java
@@ -1,47 +1,38 @@
 public class Partido {
     private Equipo equipo1;
     private Equipo equipo2;
-
     private int golesEquipo1;
     private int golesEquipo2;
 
-    public Equipo getEquipo1()
-    {
+    public Equipo getEquipo1() {
         return equipo1;
     }
 
-    public void setEquipo1(Equipo equipo1)
-    {
+    public void setEquipo1(Equipo equipo1) {
         this.equipo1 = equipo1;
     }
 
-    public Equipo getEquipo2()
-    {
+    public Equipo getEquipo2() {
         return equipo2;
     }
 
-    public void setEquipo2(Equipo equipo2)
-    {
+    public void setEquipo2(Equipo equipo2) {
         this.equipo2 = equipo2;
     }
 
-    public int getGolesEquipo1()
-    {
+    public int getGolesEquipo1() {
         return golesEquipo1;
     }
 
-    public void setGolesEquipo1(int golesEquipo1)
-    {
+    public void setGolesEquipo1(int golesEquipo1) {
         this.golesEquipo1 = golesEquipo1;
     }
 
-    public int getGolesEquipo2()
-    {
+    public int getGolesEquipo2() {
         return golesEquipo2;
     }
 
-    public void setGolesEquipo2(int golesEquipo2)
-    {
+    public void setGolesEquipo2(int golesEquipo2) {
         this.golesEquipo2 = golesEquipo2;
     }
 
@@ -52,13 +43,20 @@ public class Partido {
         this.golesEquipo2 = golesEquipo2;
     }
 
-    public Partido()
-    {
+    public Partido() {
 
     }
-    public ResultadoEnum resultado(Equipo e)
-    {
 
+    //PREGUNTAR PQ ESTA MAL REDACTADO EL DIAGRAMA DE CLASES.
+    public ResultadoEnum resultadoEquipo1() {
+        if (getGolesEquipo1()>getGolesEquipo2())return ResultadoEnum.Ganador;
+        else if (getGolesEquipo1() == getGolesEquipo2()) return ResultadoEnum.Empate;
+        return ResultadoEnum.Perdedor;
+    }
+    public ResultadoEnum resultadoEquipo2() {
+        if (getGolesEquipo1()<getGolesEquipo2())return ResultadoEnum.Ganador;
+        else if (getGolesEquipo1() == getGolesEquipo2()) return ResultadoEnum.Empate;
+        return ResultadoEnum.Perdedor;
     }
 
 }
