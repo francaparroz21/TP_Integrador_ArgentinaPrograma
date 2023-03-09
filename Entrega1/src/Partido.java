861b0d82101ef169681d99fb69c5f499fd2567cd
ErosAlexander <erosdc2018@gmail.com>
2023-03-09 15:08:46 +0100

    Se creo metodo del resultado del partido

--- Entrega1/src/Partido.java
+++ Entrega1/src/Partido.java
@@ -5,43 +5,35 @@ public class Partido {
     private int golesEquipo1;
     private int golesEquipo2;
 
-    public Equipo getEquipo1()
-    {
+    public Equipo getEquipo1() {
         return equipo1;
     }
 
-    public void setEquipo1(Equipo equipo1)
-    {
+    public void setEquipo1(Equipo equipo1) {
         this.equipo1 = equipo1;
     }
 
-    public Equipo getEquipo2()
-    {
+    public Equipo getEquipo2() {
         return equipo2;
     }
 
-    public void setEquipo2(Equipo equipo2)
-    {
+    public void setEquipo2(Equipo equipo2) {
         this.equipo2 = equipo2;
     }
 
-    public int getGolesEquipo1()
-    {
+    public int getGolesEquipo1() {
         return golesEquipo1;
     }
 
-    public void setGolesEquipo1(int golesEquipo1)
-    {
+    public void setGolesEquipo1(int golesEquipo1) {
         this.golesEquipo1 = golesEquipo1;
     }
 
-    public int getGolesEquipo2()
-    {
+    public int getGolesEquipo2() {
         return golesEquipo2;
     }
 
-    public void setGolesEquipo2(int golesEquipo2)
-    {
+    public void setGolesEquipo2(int golesEquipo2) {
         this.golesEquipo2 = golesEquipo2;
     }
 
@@ -52,12 +44,20 @@ public class Partido {
         this.golesEquipo2 = golesEquipo2;
     }
 
-    public Partido()
-    {
+    public Partido() {
 
     }
-    public ResultadoEnum resultado(Equipo e)
-    {
+
+    public ResultadoEnum resultado(Equipo e) {
+        if (e == equipo1) {
+            if (golesEquipo1 > golesEquipo2) return ResultadoEnum.Ganador;
+            else if (golesEquipo1 == golesEquipo2) return ResultadoEnum.Empate;
+            return ResultadoEnum.Perdedor;
+        } else if (e == equipo2) {
+            if (golesEquipo1 < golesEquipo2) return ResultadoEnum.Ganador;
+            else if (golesEquipo1 == golesEquipo2) return ResultadoEnum.Empate;
+            return ResultadoEnum.Perdedor;
+        }
 
     }
 
